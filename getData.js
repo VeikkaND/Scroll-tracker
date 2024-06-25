25c93b56f04db23518cc9080366f3ba793ef00f1
Veikka <veikka.nevala@outlook.com>
2024-06-25 13:46:41 +0300

    total scrolling distance & show top 5

--- getData.js
+++ getData.js
@@ -2,5 +2,22 @@ chrome.storage.local.get(null, function(obj){
     const statsArray = Object.keys(obj).map(key => {
         return {key: key, value: obj[key]}
     })
+    statsArray.sort((b, a) => {
+        if(a.value < b.value) {return -1}
+        else if(a.value > b.value) {return 1} 
+        return 0
+    })
+    console.log(statsArray)
+    document.getElementById("total")
+        .textContent = statsArray[0].value
+    const list = document.getElementById("leaderboard")
+    for(let i = 1; i < 6; i++) {
+        const item = statsArray[i]
+        const node = document.createElement("li")
+        const textnode = document
+            .createTextNode(item.key + ": " + item.value)
+        node.append(textnode)
+        list.appendChild(node)
+    }
     console.log(statsArray)
 })
--- tracker.js
+++ tracker.js
@@ -6,7 +6,24 @@ function scrollHandler(event) {
         chrome.storage.local.get(url, function(obj){
             const currentVal = Object.values(obj)[0]
             const newVal = currentVal + scrollAmount
-            chrome.storage.local.set({[url]: newVal})
+            if(newVal) {
+                chrome.storage.local.set({[url]: newVal})
+            } else {
+                chrome.storage.local.set({[url]: scrollAmount})
+            }
+            
+        })
+        chrome.storage.local.get("total", function(obj){
+            console.log(obj)
+            if(!Object.values(obj)[0]) {
+                console.log("creating new total")
+                chrome.storage.local.set({["total"]: scrollAmount})
+            } else {
+                const currentVal = Object.values(obj)[0]
+                const newVal = currentVal + scrollAmount
+                console.log("total " + newVal)
+                chrome.storage.local.set({["total"]: newVal})
+            }
         })
         //console.log(storage)
         
